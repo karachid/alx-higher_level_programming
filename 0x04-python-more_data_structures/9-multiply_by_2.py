70f8f39bf06048a5f1a77242f50927feed9d33e9
karachid <r.kabbab@gmail.com>
2023-09-13 15:17:10 -0700

    multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    n_dict = a_dictionary.copy()
+    l_keys = list(n_dict)
+
+    for i in l_keys:
+        n_dict[i] *= 2
+
+    return (n_dict)
