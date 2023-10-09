1c4506c84e96c92300d2a1372a3f3e32c6642dff
Rachid KABBAB <r.kabbab@gmail.com>
2023-10-09 16:08:57 -0700

    100 my int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines the class MyInt
+"""
+
+
+class MyInt(int):
+    def __new__(cls, *args, **kwargs):
+        """Creates a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """What was != is now =="""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """What was == is now !="""
+        return int(self) == other
