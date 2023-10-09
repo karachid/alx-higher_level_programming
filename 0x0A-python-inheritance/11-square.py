538d413c3438592acf96be7b436df85aa074020e
Rachid KABBAB <r.kabbab@gmail.com>
2023-10-09 16:06:43 -0700

    11 square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Defines the class BaseGeometry and subclass Rectangle
+"""
+
+
+class BaseGeometry:
+    """Class with public instance methods area and integer_validator"""
+    def area(self):
+        """Raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Representation of a rectangle"""
+    def __init__(self, width, height):
+        """Instantiates of the rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """Representation of a square"""
+    def __init__(self, size):
+        """Instantiates of the square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """"Returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Informal string representation of the square"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
