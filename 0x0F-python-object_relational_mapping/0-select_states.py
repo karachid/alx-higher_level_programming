863034b185b5e859b5d03235a3ebeb8884198eb0
Rachid KABBAB <r.kabbab@gmail.com>
2023-12-16 05:05:55 -0800

    0 select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+rows = cur.fetchall()
+for r in rows:
+    print(r)
+cur.close()
+db.close()
