62ecfd8e0645ff752c589c5a7d9f326642507e05
Rachid KABBAB <r.kabbab@gmail.com>
2023-12-16 10:26:00 -0800

    2 my filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" a script that lists all states with a name starting
-    with N (upper N) from the database hbtn_0e_0_usa
+""" A script that takes in an argument and displays all values
+    in the states table of hbtn_0e_0_usa
+    where name matches the argument
 """
 import MySQLdb
 import sys
@@ -10,7 +11,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for r in rows:
         print(r)
