da5ef0efb32d748f26d0cc70e31da956cfdc6db2
Rachid KABBAB <r.kabbab@gmail.com>
2023-12-16 11:42:40 -0800

    Fixed a pystyle issues

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT c.id, c.name, s.name FROM states s, cities c WHERE c.state_id=s.id")
+    cur.execute("SELECT c.id, c.name, s.name FROM
+                states s, cities c WHERE c.state_id=s.id")
     rows = cur.fetchall()
     for r in rows:
         print(r)
