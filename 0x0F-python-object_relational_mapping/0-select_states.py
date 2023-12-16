c4ac97251d4bb01b3ba3aa25974381b1d16c310e
Rachid KABBAB <r.kabbab@gmail.com>
2023-12-16 05:12:20 -0800

    Removed group by clause from the query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3], port=3306)
 cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
+cur.execute("SELECT * FROM states")
 rows = cur.fetchall()
 for r in rows:
     print(r)
