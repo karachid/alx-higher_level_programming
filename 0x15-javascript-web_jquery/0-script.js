92a88c1357beb00c0c61b572551933f4d75bae27
Rachid KABBAB <r.kabbab@gmail.com>
2024-02-07 13:41:29 -0800

    New version of Task 0

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,3 @@
-(function () {
-  document.querySelector('header').style.color = '#FF0000';
-})();
+//change text color of header element
+const header = document.querySelector("header");
+header.style.color = "#FF0000";
